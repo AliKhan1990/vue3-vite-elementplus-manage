e2b691c6cb65a2aadfc98834c8c104355a02770f
Ali Khan <aliomarkhan@sina.cn>
2023-06-20 17:59:48 +0800

    perf(axios): axios response 拦截器配置

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -23,7 +23,7 @@ export default {
           password: md5(password)
         })
           .then(data => {
-            this.commit('user/setToken', data.data.data.token)
+            this.commit('user/setToken', data.token)
             resolve(data)
           })
           .catch(err => {
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { ElMessage } from 'element-plus'
 
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API,
@@ -14,6 +15,26 @@ service.interceptors.request.use(
     config.headers.icode = '40C9D047C730848F'
     // 必须返回 config
     return config
+  }
+)
+
+// 响应拦截器
+service.interceptors.response.use(
+  response => {
+    const { success, message, data } = response.data
+    //   要根据success的成功与否决定下面的操作
+    if (success) {
+      return data
+    } else {
+      // 业务错误
+      ElMessage.error(message) // 提示错误消息
+      return Promise.reject(new Error(message))
+    }
+  },
+  error => {
+    // TODO: 将来处理 token 超时问题
+    ElMessage.error(error.message) // 提示错误信息
+    return Promise.reject(error)
   }
 )
 
