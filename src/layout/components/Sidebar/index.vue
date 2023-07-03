0edd55e868d1a15d2bd040144367ab92c276c7ab
Ali Khan <aliomarkhan@sina.cn>
2023-07-03 17:51:38 +0800

    perf(左侧navbar): 左侧头像及用户名设置

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="sidebar-container">
-    <h1>哈哈哈哈</h1>
+    <div :class="['avatar-container', $store.getters.sidebarOpened ? 'scale-up' : 'scale-down']">
+      <el-avatar :size="$store.getters.sidebarOpened ? 'default' : 'small'" shape="square" src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png" />
+      <p class="username" v-if="$store.getters.sidebarOpened">{{$store.getters.userInfo.username }}</p>
+    </div>
     <el-scrollbar>
       <side-bar-menu />
     </el-scrollbar>
@@ -9,6 +12,30 @@
 
 <script setup>
 import SideBarMenu from './SideBarMenu'
+// import variables from '@/styles/variables.module.scss'
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.username {
+  color: white;
+}
+.avatar-container {
+  padding-top: 15px;
+}
+.logo-container {
+  height: 44px;
+  padding: 10px 0 22px 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .logo-title {
+    margin-left: 10px;
+    color: #fff;
+    font-weight: 600;
+    line-height: 50px;
+    font-size: 16px;
+    white-space: nowrap;
+  }
+}
+</style>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -42,14 +42,6 @@ import AppMain from './components/AppMain'
   right: 0;
   z-index: 9;
   width: calc(100% - #{$sideBarWidth});
-}
-
-.fixed-header {
-  position: fixed;
-  top: 0;
-  right: 0;
-  z-index: 9;
-  width: calc(100% - #{$sideBarWidth});
   transition: width 0.28s;
 }
 
